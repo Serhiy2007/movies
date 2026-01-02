f56c9e219410b6721d2aa40491368eb0e8eb46cb
Serhiy2007 <serhiyogorodnuk@gmail.com>
2026-01-02 10:26:17 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// Ефект появи секцій при прокручуванні
+const elements = document.querySelectorAll(".card, h2, p");
+
+function reveal() {
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            el.style.opacity = "1";
+            el.style.transform = "translateY(0)";
+        }
+    });
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+
